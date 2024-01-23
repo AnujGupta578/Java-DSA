e938b1d8642d1040b8dd9add2d7b5bc8f576e74a
Anuj Kumar Gupta <anujkumar.gupta@clarivate.com>
2024-01-23 07:31:56 +0530

    Bubble sort

---
+++ src/main/java/tutorials/Sorting/BubbleSort.java
@@ -0,0 +1,38 @@
+package tutorials.Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1,8,7,3,6,8,4,5};
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr) {
+
+        boolean swapped;
+
+//        run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+//            for each step, max item will come at the last respective index
+            for (int j = 1; j < arr.length - i ; j++) {
+//                swap if the item is smaller than the previous one.
+
+                if(arr[j] < arr[j-1]) { // 5<10 -> j=0 -> 10 & j=1 -> 5 [10, 5] -> j = 1, j-1 = 0
+                    int temp = arr[j]; // temp = 5;
+                    arr[j] = arr[j-1];  // 5 -> 10
+                    arr[j-1] = temp; // 10 -> 5 => [5, 10]
+                    swapped = true;
+                }
+            }
+//            if no swapped occur break the loop
+            if(!swapped) {
+                break;
+            }
+        }
+    }
+}
